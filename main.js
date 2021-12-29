c96daf6fd979ed5ff37ee778dc749e42af79f6e0
avpimblesr <allan.pimble@gmail.com>
2021-12-28 20:34:49 -0500

    created a 'temporary player'

--- main.js
+++ main.js
@@ -10,6 +10,27 @@ function computerPlay() {
     return "Scissors";
   }
 }
-console.clear();
-console.log(computerPlay());
-console.log();
+
+function playerPlay() {
+  //  randomly return either 'Rock', 'Paper' or 'Scisors'
+  //  replace the ramdom selection with live player interaction
+  //  once the program is near completion
+  const theSelection = Math.floor((Math.random() * 3) + 1)
+
+  if(theSelection === 1) {
+    return "Rock";
+  } else if (theSelection === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+
+playerSelection = playerPlay()
+computerSelection = computerPlay()
+
+playRockPaperScissors(playerSelection, computerSelection)
+
+function playRockPaperScissors(playerSelection, computerSelection) {
+}
